ad3e0e247c8faa4d8e1e73977d62775cf0298c44
bartoszzudzin <b.zudzin@gmail.com>
2023-10-14 19:22:58 +0200

    Initial commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () =>{
     return(
         <footer>
-            <p>Scandiweb Test assignment</p>
+            <p>eCommerce App by Bartosz Zudzin</p>
         </footer>
     )
 }
